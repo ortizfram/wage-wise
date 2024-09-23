60556b58324a7b69504e44b7db89259161eb44d9
Franco Ortiz <ortizfranco48@yahoo.com>
2024-09-23 12:32:18 -0300

    feriado regular report

--- app/[empId]/report.jsx
+++ app/[empId]/report.jsx
@@ -5,6 +5,8 @@ import {
   View,
   Pressable,
   Platform,
+  TextInput,
+  ScrollView,
 } from "react-native";
 import { fetchEmployeeWithId } from "../../services/organization/fetchEmployees";
 import { fetchShiftWithId } from "../../services/userShift/fetchShifts";
@@ -23,6 +25,13 @@ const Report = () => {
   const [endDate, setEndDate] = useState(new Date());
   const [showStartDatePicker, setShowStartDatePicker] = useState(false);
   const [showEndDatePicker, setShowEndDatePicker] = useState(false);
+  const [totalPayment, setTotalPayment] = useState(0);
+  const [holidayCost, setHolidayCost] = useState(0);
+  const [excedente, setExcedente] = useState(0);
+  const [totalCost, setTotalCost] = useState(0);
+
+  const [bonus, setBonus] = useState(0);
+  const [advance, setAdvance] = useState(0);
 
   useEffect(() => {
     const loadEmployee = async () => {
@@ -42,7 +51,7 @@ const Report = () => {
           endDate.toISOString().split("T")[0]
         );
         setShifts(data);
-        calculateTotalHours(data); // Calcular horas totales
+        calculateTotalHours(data);
       } catch (error) {
         console.error("Error fetching shifts:", error);
       }
@@ -50,7 +59,7 @@ const Report = () => {
 
     loadEmployee();
     loadShiftForEmployee();
-  }, [empId, startDate, endDate]);
+  }, [empId, startDate, endDate, bonus, advance]);
 
   const onStartDateChange = (event, selectedDate) => {
     if (Platform.OS === "web") {
@@ -75,102 +84,183 @@ const Report = () => {
   const calculateTotalHours = (shifts) => {
     let hours = 0;
     let minutes = 0;
+    let holidayMinutes = 0;
 
     shifts.forEach((shift) => {
-      // Suponiendo que shift.total_hours es algo como "2h 45m"
       const [h, m] = shift.total_hours.split(" ");
       hours += parseInt(h.replace("h", ""));
       minutes += parseInt(m.replace("m", ""));
+
+      if (shift.shift_mode === "holiday") {
+        holidayMinutes +=
+          parseInt(h.replace("h", "")) * 60 + parseInt(m.replace("m", ""));
+      }
     });
 
-    // Si los minutos suman más de 60, convertir en horas
     hours += Math.floor(minutes / 60);
     minutes = minutes % 60;
 
     setTotalHours(hours);
     setTotalMinutes(minutes);
+
+    const totalWorkedMinutes = hours * 60 + minutes;
+    const hourlyFee = employee.hourly_fee || 0;
+    const travelCost = employee.travel_cost || 0;
+
+    const regularCost = (totalWorkedMinutes / 60) * hourlyFee;
+    const holidayCostValue = (holidayMinutes / 60) * hourlyFee;
+    setHolidayCost(holidayCostValue);
+
+    const totalCostValue =
+      regularCost +
+      holidayCostValue +
+      travelCost +
+      parseFloat(bonus) -
+      parseFloat(advance);
+    setTotalCost(totalCostValue.toFixed(2));
+
+    const declaredHours = employee.declared_hours || 0;
+    const excedente = declaredHours ? hours - declaredHours : 0;
+    setExcedente(excedente);
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Reporte de Horas</Text>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Reporte de Horas</Text>
 
-      {employee && employee.email ? (
-        <View>
-          <Text style={styles.employeeText}>{employee.email}</Text>
-          <Text style={styles.title}>
-            Horas Totales: {totalHours}h {totalMinutes}m
+        {employee && employee.email ? (
+          <View>
+            {employee.firstname && employee.lastname ? (
+              <Text style={styles.employeeText}>
+                {employee.firstname} {employee.lastname}
+              </Text>
+            ) : (
+              <View></View>
+            )}
+
+            <Text style={styles.employeeText}>{employee.email}</Text>
+            <View>
+              <Text style={styles.title}>
+                Horas Totales: {totalHours}h {totalMinutes}m
+              </Text>
+              <Text style={styles.employeeText}>
+                Tarifa Horaria: ${employee.hourly_fee || 0} | Costo Viaje: $
+                {employee.travel_cost || 0} |{` Excedente: ${excedente}hs`} |
+                Feriados: ${holidayCost || 0}
+              </Text>
+
+              <View style={styles.editableRow}>
+                <Text style={styles.label}>Premio:</Text>
+                <TextInput
+                  style={styles.input}
+                  keyboardType="numeric"
+                  value={String(bonus)}
+                  onChangeText={setBonus}
+                  placeholder="Ingrese premio"
+                />
+              </View>
+
+              <View style={styles.editableRow}>
+                <Text style={styles.label}>Adelanto:</Text>
+                <TextInput
+                  style={styles.input}
+                  keyboardType="numeric"
+                  value={String(advance)}
+                  onChangeText={setAdvance}
+                  placeholder="Ingrese adelanto"
+                />
+              </View>
+
+              <Text style={styles.largeText}>Total: ${totalCost}</Text>
+            </View>
+          </View>
+        ) : (
+          <Text style={styles.errorText}>
+            No se encontraron datos del empleado
           </Text>
-        </View>
-      ) : (
-        <Text style={styles.errorText}>
-          No se encontraron datos del empleado
-        </Text>
-      )}
+        )}
 
-      {Platform.OS !== "web" && (
-        <>
-          <Pressable
-            style={styles.button}
-            onPress={() => setShowStartDatePicker(true)}
-          >
-            <Text style={styles.buttonText}>Seleccionar Fecha de Inicio</Text>
-          </Pressable>
-          <Pressable
-            style={styles.button}
-            onPress={() => setShowEndDatePicker(true)}
-          >
-            <Text style={styles.buttonText}>Seleccionar Fecha de Fin</Text>
-          </Pressable>
-        </>
-      )}
+        {Platform.OS !== "web" && (
+          <>
+            <Pressable
+              style={styles.button}
+              onPress={() => setShowStartDatePicker(true)}
+            >
+              <Text style={styles.buttonText}>Seleccionar Fecha de Inicio</Text>
+            </Pressable>
+            <Pressable
+              style={styles.button}
+              onPress={() => setShowEndDatePicker(true)}
+            >
+              <Text style={styles.buttonText}>Seleccionar Fecha de Fin</Text>
+            </Pressable>
+          </>
+        )}
 
-      {showStartDatePicker && Platform.OS !== "web" && (
-        <DateTimePicker
-          value={startDate}
-          mode="date"
-          display="default"
-          onChange={onStartDateChange}
-        />
-      )}
+        {showStartDatePicker && Platform.OS !== "web" && (
+          <DateTimePicker
+            value={startDate}
+            mode="date"
+            display="default"
+            onChange={onStartDateChange}
+          />
+        )}
 
-      {showEndDatePicker && Platform.OS !== "web" && (
-        <DateTimePicker
-          value={endDate}
-          mode="date"
-          display="default"
-          onChange={onEndDateChange}
-        />
-      )}
+        {showEndDatePicker && Platform.OS !== "web" && (
+          <DateTimePicker
+            value={endDate}
+            mode="date"
+            display="default"
+            onChange={onEndDateChange}
+          />
+        )}
 
-      {Platform.OS === "web" && (
-        <View>
-          <Text style={styles.label}>Seleccionar Fecha de Inicio</Text>
-          <DatePicker selected={startDate} onChange={onStartDateChange} />
-          <Text style={styles.label}>Seleccionar Fecha de Fin</Text>
-          <DatePicker selected={endDate} onChange={onEndDateChange} />
-        </View>
-      )}
+        {Platform.OS === "web" && (
+          <View>
+            <Text style={styles.label}>Seleccionar Fecha de Inicio</Text>
+            <DatePicker selected={startDate} onChange={onStartDateChange} />
+            <Text style={styles.label}>Seleccionar Fecha de Fin</Text>
+            <DatePicker selected={endDate} onChange={onEndDateChange} />
+          </View>
+        )}
 
-      {shifts.length > 0 ? (
-        shifts.map((shift, index) => (
-          <Text key={index} style={styles.shiftText}>
-            Día: {shift.date} - Entrada: {shift.in} - Salida: {shift.out} -
-            Horas: {shift.total_hours}
+        {shifts.length > 0 ? (
+          shifts.map((shift, index) => (
+            <View key={index} style={styles.shiftContainer}>
+              {shift.shift_mode === "holiday" ? (
+                <View style={styles.star}>
+                  <Text style={styles.starText}>F</Text>
+                </View>
+              ) : (
+                <View style={styles.circle}>
+                  <Text style={styles.circleText}>R</Text>
+                </View>
+              )}
+              <Text style={styles.shiftText}>
+                {shift.date} -{" "}
+                <Text style={{ color: "green" }}>Entrada: {shift.in}</Text> -{" "}
+                <Text style={{ color: "red" }}>Salida: {shift.out}</Text> -{" "}
+                Horas: {shift.total_hours}
+              </Text>
+            </View>
+          ))
+        ) : (
+          <Text style={styles.errorText}>
+            No se encontraron turnos para las fechas seleccionadas
           </Text>
-        ))
-      ) : (
-        <Text style={styles.errorText}>
-          No se encontraron turnos para las fechas seleccionadas
-        </Text>
-      )}
-    </View>
+        )}
+      </View>
+    </ScrollView>
   );
 };
 
 export default Report;
-
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
     padding: 20,
@@ -179,40 +269,98 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "bold",
-    textAlign: "center",
     marginBottom: 20,
+    textAlign: "center",
   },
   employeeText: {
     fontSize: 18,
-    textAlign: "center",
     marginBottom: 10,
+    textAlign: "center",
+  },
+  largeText: {
+    fontSize: 20,
+    marginTop: 10,
+    textAlign: "center",
   },
   errorText: {
-    fontSize: 16,
     color: "red",
+    marginBottom: 10,
     textAlign: "center",
+  },
+  editableRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginBottom: 10,
   },
   label: {
+    fontSize: 18,
+    fontWeight: "bold",
+    flex: 1,
+    textAlign: "left",
+  },
+  input: {
+    flex: 2,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    paddingHorizontal: 10,
     fontSize: 16,
-    marginVertical: 10,
+    height: 40,
+    backgroundColor: "#fff",
   },
   button: {
     backgroundColor: "#007BFF",
-    paddingVertical: 12,
-    paddingHorizontal: 25,
-    borderRadius: 10,
-    marginVertical: 10,
-    alignItems: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 10,
   },
   buttonText: {
     color: "#fff",
     fontSize: 16,
-    fontWeight: "bold",
+    textAlign: "center",
   },
   shiftText: {
     fontSize: 16,
     marginVertical: 5,
     textAlign: "center",
+  },
+  shiftContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 5,
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: "#007BFF",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
+  },
+  circleText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  star: {
+    width: 30,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
+    backgroundColor: "yellow", // Make the background yellow
+    borderRadius: 15, // This creates a circular background
+    overflow: "hidden",
+    position: "relative",
+  },
+  starText: {
+    color: "black", // Set text color to black for contrast
+    fontSize: 18,
+    fontWeight: "900",
+    position: "absolute",
+    top: "15%", // Adjust position for better alignment
+    left: "35%",
   },
 });
